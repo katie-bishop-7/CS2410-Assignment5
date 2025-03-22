b196402f7deef0819612d90ce92061b5568b07d7
Katie Bishop <katiebishop207@gmail.com>
2025-03-22 13:12:30 -0600

    fixed bug in server

--- server.py
+++ server.py
@@ -1,6 +1,11 @@
+# You don't need to modify this if you don't want to
+# Start with: python server.py
+# localhost:8000 is the url to access the server
+
 import re
 import os
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import mimetypes
 
 filename = "favs.txt"
 
@@ -8,7 +13,7 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
     def do_PUT(self):
         if re.search('/api/update-favs', self.path):
             length = int(self.headers.get('content-length'))
-            data = self.rfile.read(length).decode('utf8')
+            data = self.rfile.read(length).decode('utf-8')
 
             # Write the data to the file
             f = open(filename, "w")
@@ -22,17 +27,26 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         path_without_query = self.path.split('?')[0]
-        file_requested = path_without_query.split('/')[-1]
+        file_requested = path_without_query.lstrip('/')  # Remove leading slash
 
         if os.path.isfile(file_requested):
             self.send_response(200)
-            self.send_header('Content-Type', 'text.html')
-            self.end_headers()
-            self.wfile.write(bytes(open(file_requested).read(), 'utf-8'))
 
+            # Guess content type based on file extension
+            content_type, _ = mimetypes.guess_type(file_requested)
+            if content_type is None:
+                content_type = 'application/octet-stream'
+
+            self.send_header('Content-Type', content_type)
+            self.end_headers()
+
+            # Open the file in binary mode
+            with open(file_requested, 'rb') as f:
+                self.wfile.write(f.read())
         else:
             self.send_response(404, 'Not Found: record does not exist')
             self.end_headers()
+
 
         
 if __name__ == '__main__':
