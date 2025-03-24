372e8ff4cfa07f504ccf4afc791cade4113c6140
Katie Bishop <katiebishop207@gmail.com>
2025-03-24 13:12:44 -0600

    gallery, unfavorite from gallery

--- gallery.js
+++ gallery.js
@@ -1,13 +1,73 @@
-console.log("Javascript Gallery loaded")
-
 let gallery_cards = document.getElementById("gallery")
 
-const url = "http://localhost:8000/";
-const filename = "favs.txt";
-
-
-fetch(url + filename)
+fetch("http://localhost:8000/favs.txt")
     .then(res => res.json())
-    .then(res => {
-        console.log (res.url);
+    .then(favs => {
+        let heart_counter = 1;
+        for (link in favs) {
+            let divContainer = document.createElement("div")
+            divContainer.className = "gallery_card"
+            
+
+            let image = document.createElement("img")
+            image.className = "image_gallery_card"
+            image.src = link
+            image.alt = "image gallery card"
+
+            let imageDiv = document.createElement("div")
+            imageDiv.className = "image-div"
+            let apiDiv = document.createElement("div")
+            apiDiv.className = "image-info"
+            apiDiv.innerHTML = "<b>API Link:</b> "
+            apiDiv.innerHTML += favs[link]["api-link"]
+
+            let imageLinkDiv = document.createElement("div")
+            imageLinkDiv.className = "image-info"
+            imageLinkDiv.innerHTML = "<b>Image Link:</b> "
+            imageLinkDiv.innerHTML += link
+
+            let dateDiv = document.createElement("div")
+            dateDiv.className = "image-info"
+            dateDiv.innerHTML = "<b>Date Added:</b> "
+            dateDiv.innerHTML += favs[link]["date-added"]
+
+            let heart = document.createElement("img")
+            heart.className = "heart"
+            heart.id = link
+            heart.src = "filled-heart-glow.png"
+            heart.alt = "heart"
+
+            heart.addEventListener("click", (e) => {
+                e.preventDefault()
+                heart.src = "empty-heart-glow.png"
+                fetch("http://localhost:8000/favs.txt")
+                    .then(curr_data => curr_data.json())
+                    .then(curr_data_json => {
+                        delete curr_data_json[heart.id]
+                        gallery_cards.removeChild(divContainer)
+                        console.log(curr_data_json)
+                        return fetch("http://localhost:8000/api/update-favs", {
+                            method: "PUT",
+                            body: JSON.stringify(curr_data_json),
+                            headers: {
+                                "Content-type": "application/json; charset=UTF-8"
+                            }
+                        })
+                        
+                    })
+            }
+            )
+            heart_counter += 1;
+
+            divContainer.appendChild(image)
+            imageDiv.appendChild(apiDiv)
+            imageDiv.appendChild(imageLinkDiv)
+            imageDiv.appendChild(dateDiv)
+            divContainer.appendChild(imageDiv)
+            divContainer.appendChild(heart)
+
+            gallery_cards.appendChild(divContainer)
+        }
+        // create new card. three divs nested inside one div
+        // console.log(res)
     });
--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-
 console.log("Main loaded")
 heart = document.getElementById("heart")
 random_image = document.getElementById("random")
+image_link = document.getElementById("image-link")
+api_link = document.getElementById("api-link")
 
-// async function getData(){
-//     const res = await fetch("favs.txt")
-//     const text = await res.text();
-//     return text
-// }
+// set random image upon loading
+let api_url = find_api_url()
+let image_url = fetch_image(api_url).then(url => {
+    random_image.src = url.url
+    random_image.alt = "Random image"
+    image_link.innerText = url.url
+    api_link.innerText = api_url
+}
+)
 
 // make the heart favorite and unfavorite
 heart.addEventListener("click", e => {
@@ -20,54 +25,76 @@ heart.addEventListener("click", e => {
     else if (favorited === "false") {
         heart.setAttribute("data-favorited", "true");
         heart.src = "filled-heart-glow.png"
+        // add image to favs.txt
+        fetch("http://localhost:8000/favs.txt")
+            .then(curr_data => curr_data.json())
+            .then(curr_data_json => {
+                curr_data_json[document.getElementById("image-link").innerText] = {
+                    "api-link": document.getElementById("api-link").innerText,
+                    "date-added": new Date()
+                }
+                return fetch("http://localhost:8000/api/update-favs", {
+                    method: "PUT",
+                    body: JSON.stringify(curr_data_json),
+                    headers: {
+                        "Content-type": "application/json; charset=UTF-8"
+                    }
+                })
+            })
     }
-    // add image to favs.txt
+}
+)
 
-    const favs_data = {
-        url: "cake.jpg",
-    }
-    fetch("http://localhost:8000/api/update-favs", {
-        method: "PUT",
-        body: JSON.stringify(favs_data),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
-    });
-})
-
-
-// regenerate image
-random_image = document.getElementById("random")
+// regenerate image when you click regenerate button
 
 regenerate_button = document.getElementById("regenerate")
 regenerate_button.addEventListener("click", e => {
     e.preventDefault()
+    regenerate_image()
+})
+
+// If category is changed, regenerate the image
+
+image_category = document.getElementById("category")
+image_category.addEventListener("change", e => {
+    regenerate_image()
+})
+
+// generate the url for the api based on category and WIDTH
+
+function find_api_url() {
+    let category = document.getElementById("category").value
+    let WIDTH = 1500;
+    let fetch_url = `https://picsum.photos/${WIDTH}`
+    if (category !== "1") {
+        fetch_url += `/${category}`
+    }
+    return fetch_url
+}
+
+// fetch image url from api (async)
+
+async function fetch_image(fetch_url) {
+    const result = await fetch(fetch_url)
+    const pic_url = await result
+    return pic_url
+}
+
+// regenerate the main image (calls async fetch_image)
+
+function regenerate_image() {
     regenerate_button.innerText = "Regenerating..."
-    let res = regenerate_image(document.getElementById("category").value)
-    res.then(result => {
+    let api_url = find_api_url()
+    let image_url = fetch_image(api_url)
+    image_url.then(result => {
         random_image.src = result.url
+        image_link.innerText = result.url
         regenerate_button.innerText = "Regenerate"
         heart.setAttribute("data-favorited", "false");
         heart.src = "empty-heart-glow.png"
     },)
-})
+    api_link = document.getElementById("api-link")
+    image_link = document.getElementById("image-link")
+    api_link.innerText = api_url
 
-// assign category to regenerate image
-
-image_category = document.getElementById("category")
-image_category.addEventListener("change", e => {
-    e.preventDefault();
-    let category = e.target.value;
-    console.log(category)
-})
-
-// fetch image from api
-async function regenerate_image(category) {
-    let fetch_url = "https://picsum.photos/1500"
-    if (category !== "1") {
-        fetch_url += `/${category}`
-    }
-    const result = await fetch(fetch_url)
-    const data = await result
-    return data
 }
